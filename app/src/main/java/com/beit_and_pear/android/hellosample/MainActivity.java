bc54d81bdf65e35d0f3cafb550541fa5748225d8
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2021-12-26 21:37:54 +0900

    Clearボタンの追加

--- app/src/main/java/com/beit_and_pear/android/hellosample/MainActivity.java
+++ app/src/main/java/com/beit_and_pear/android/hellosample/MainActivity.java
@@ -14,10 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // 表示ボタンが押された時
         Button btClick = findViewById(R.id.btClick);
         HelloListener listener = new HelloListener();
         btClick.setOnClickListener(listener);
+        // クリアボタンが押された時
+        Button btClear = findViewById(R.id.btClear);
+        btClear.setOnClickListener(listener);
     }
 
     private class HelloListener implements View.OnClickListener {
@@ -25,8 +28,19 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             EditText input = findViewById(R.id.etName);
             TextView output = findViewById(R.id.tvOutput);
-            String inputStr = input.getText().toString();
-            output.setText(inputStr + "さん、こんにちは！");
+
+            int id = v.getId();
+
+            switch (id) {
+                case R.id.btClick:
+                    String inputStr = input.getText().toString();
+                    output.setText(inputStr + "さん、こんにちは！");
+                    break;
+                case R.id.btClear:
+                    output.setText("");
+                    break;
+            }
+
         }
     }
 }
